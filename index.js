33297ec5b55217c8a6fd8c4a07bc8f8560282b62
LonelyChimo302 <t2.handtke@gmail.com>
2023-07-28 22:27:35 +0200

    update

--- index.js
+++ index.js
@@ -68,14 +68,13 @@ for (const file of eventFiles) {
 	}
 }
 
+// Log in to Discord with your client's token
+
+client.login(token);
+
 // Set Playing Status
 
 setInterval(function(){
     let status = statuses[Math.floor(Math.random() * statuses.length)]
     client.user.setActivity(status, { type: 'WATCHING' })
 }, 600000);
-
-
-// Log in to Discord with your client's token
-
-client.login(token);
