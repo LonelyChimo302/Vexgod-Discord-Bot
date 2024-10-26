2e2f59cecad33f0f70350353ee723caab4ad8838
LonelyChimo302 <t2.handtke@gmail.com>
2024-10-26 17:21:21 +0200

    Hawk Tuah Flo edit

--- events/HawkTuah.js
+++ events/HawkTuah.js
@@ -8,8 +8,11 @@ module.exports = {
 
         var messagetext = message.content
 
-        if (messagetext === "Hawk Tuah"){
+        if (messagetext === "Hawk Tuah" && message.author.id === "282082952026783744"){
+            await message.reply('Spit on that Florian!')
+        }
+        else if (messagetext === "Hawk Tuah"){
             await message.reply('Spit on that thang!')
-        } 
+        }
 	},
 };
