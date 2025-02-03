6f05866ea007d8bc58315045f07d766d46a37cb3
LonelyChimo302 <t2.handtke@gmail.com>
2025-02-03 21:36:56 +0100

    possible fix?

--- events/EmoteRequest.js
+++ events/EmoteRequest.js
@@ -2,35 +2,42 @@ const { Events } = require('discord.js');
 
 const requestchannel = '1236748227274084382';
 
+const maraID = '1134558655098916974'
+
 module.exports = {
     name: Events.MessageCreate,
     once: false,
     async execute(message) {
 
-        if (message.author.bot) return;
-
         if (message.channel.id === requestchannel) {
 
-            await message.reply({
-                poll: {
-                    question: {
-                        text: 'Soll das zum Emote/Sticker/Soundboard Sound werden?',
+            if (message.member.id === maraID) {
+
+            }
+
+            else {
+                await message.reply({
+                    poll: {
+                        question: {
+                            text: 'Soll das zum Emote/Sticker/Soundboard Sound werden?',
+                        },
+                        answers: [
+                            {
+                                text: 'Yee',
+                                emoji: '1116464522379665458',
+                            },
+                            {
+                                text: 'Nö',
+                                emoji: '1116464573797638145',
+                            },
+                        ],
+                        duration: 48, // In Stunden
+                        allowMultiselect: false,
+                        layoutType: 1, // Single type (optional)
                     },
-                    answers: [
-                        {
-                            text: 'Yee',
-                            emoji: '1116464522379665458',
-                        },
-                        {
-                            text: 'Nö',
-                            emoji: '1116464573797638145',
-                        },
-                    ],
-                    duration: 48, // In Stunden
-                    allowMultiselect: false,
-                    layoutType: 1, // Single type (optional)
-                },
-            })
+                })
+            }
         }
     }
+
 }
